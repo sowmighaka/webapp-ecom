0a6ba789961f3091f577f20f6927525fba821723
sowmighaka <sowmigha.ka2024aiml@sece.ac.in>
2025-12-18 15:44:05 +0530

    Update Register.jsx

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -15,7 +15,7 @@ export default function Register({ onRegister }) {
       return;
     }
 
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("http://16.171.242.80:5000/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
