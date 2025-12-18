cc578b172014d1afe9056abcf07e4e5d3bdaa519
sowmighaka <sowmigha.ka2024aiml@sece.ac.in>
2025-12-18 15:46:17 +0530

    Update Products.jsx

--- frontend/src/Products.jsx
+++ frontend/src/Products.jsx
@@ -17,7 +17,7 @@ export default function Products({ addToCart }) {
       const token = localStorage.getItem("token");
 
       try {
-        const res = await fetch("http://localhost:5000/api/products", {
+        const res = await fetch("http://16.171.242.80:5000/api/products", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
